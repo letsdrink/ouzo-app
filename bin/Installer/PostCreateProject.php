ad0acbd1ee8a0c37e7984f5076c6d18b5aa69539
piotrooo <piotroo89@gmail.com>
2013-10-14 14:47:59 +0200

    Start working with post install scripts.

--- bin/Installer/PostCreateProject.php
+++ bin/Installer/PostCreateProject.php
@@ -7,9 +7,10 @@ class PostCreateProject
 {
     public static function changePrefix(Event $event)
     {
-        $prefixSystem = $event->getComposer()->getPackage()->getTargetDir();
+        $prefixSystem = $event->getComposer()->getPackage();
         $a = print_r($prefixSystem, true);
         $b = print_r(__DIR__, true);
-        file_put_contents('/tmp/test', $a . $b);
+        file_put_contents('/tmp/test1', $a);
+        file_put_contents('/tmp/test2', $b);
     }
 }
