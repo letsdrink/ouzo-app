defc6fc14b4a804b7c3987ee62a780cfe2776344
bbankowski <bbankowski@gmail.com>
2013-11-02 16:01:47 +0100

    Removed not needed config params.

--- config/prod/ConfigPanel.php
+++ config/prod/ConfigPanel.php
@@ -6,7 +6,7 @@ $config['db']['driver'] = 'pgsql';
 $config['db']['host'] = '127.0.0.1';
 $config['db']['port'] = '5432';
 $config['global']['prefix_system'] = '/ouzo-test';
-$config['debug'] = true;
 $config['sql_dialect'] = '\\Ouzo\\Db\\Dialect\\PostgresDialect';
+$config['debug'] = true;
 
 return $config;
