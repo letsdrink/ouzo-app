4996f4448156823ffa6391e7538b8c5e40a6ba92
piotrooo <piotroo89@gmail.com>
2014-09-02 11:31:49 +0200

    Update to dev-master.

--- bootstrap_test.php
+++ bootstrap_test.php
@@ -1,5 +1,4 @@
 <?php
-use Ouzo\Loader;
 use Ouzo\Utilities\Clock;
 use Ouzo\Utilities\Files;
 use Ouzo\Utilities\Path;
@@ -12,21 +11,7 @@ define('ROOT_PATH', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 
 require 'vendor/autoload.php';
 
-Files::loadIfExists(Path::join(ROOT_PATH, 'vendor', 'letsdrink', 'ouzo', 'src', 'Ouzo', 'Loader.php'));
-Files::loadIfExists(Path::join(ROOT_PATH, 'vendor', 'letsdrink', 'ouzo', 'src', 'Ouzo', 'FrontController.php'));
-Files::loadIfExists(Path::join(ROOT_PATH, 'vendor', 'letsdrink', 'ouzo', 'src', 'Ouzo', 'ExceptionHandling', 'ErrorHandler.php'));
-
 Files::loadIfExists(Path::join(ROOT_PATH, 'config', 'error_codes.php'));
 Files::loadIfExists(Path::join(ROOT_PATH, 'config', 'routes.php'));
-
-$loader = new Loader();
-$loader
-    ->setIncludePath('custom/')
-    ->setIncludePath('application/')
-    ->setIncludePath('locales/')
-    ->setIncludePath('test/application/')
-    ->setIncludePath('test/seed/')
-    ->setIncludePath('seed/')
-    ->register();
 
 Clock::freeze();
