7ef43d65e92b39df1954a717d5c25da6ab06530d
Bartosz Bańkowski <bbankowski@gmail.com>
2014-12-14 21:21:44 +0100

    Changed dir names to comply with new Ouzo.

---
+++ Application/Helper/UrlHelper.php
@@ -0,0 +1,2 @@
+<?php
+include_once 'GeneratedUriHelper.php';
---
+++ Application/Model/User.php
@@ -0,0 +1,21 @@
+<?php
+namespace Application\Model;
+
+use Ouzo\Model;
+
+class User extends Model
+{
+    function __construct($attributes = array())
+    {
+        parent::__construct(array(
+            'attributes' => $attributes,
+            'fields' => array('login', 'password')));
+    }
+
+    public function validate()
+    {
+        parent::validate();
+        $this->validateNotBlank($this->login, 'Login cannot be blank');
+    }
+
+}
