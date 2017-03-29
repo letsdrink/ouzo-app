283136c4a434a1f0c92b12504c1b527c5b2ff3cb
Bartosz Bańkowski <bbankowski@gmail.com>
2017-03-29 20:13:31 +0200

    Cleanup for PHP 5.6.

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,6 @@
 <?php
 use Ouzo\Routing\Route;
+
 Route::get('/', 'users#index');
 Route::allowAll('/hello_world', 'hello_world');
 Route::resource('users');
--- locales/en.php
+++ locales/en.php
@@ -1,8 +1,8 @@
 <?php
 
-return array(
-    'user' => array(
+return [
+    'user' => [
         'login' => 'login',
         'password' => 'password'
-    )
-);
+    ]
+];
